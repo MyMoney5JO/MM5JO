7588495d3947ee701a662ce32294c3f230aa4c57
soo-jin-lim <tn7969@naver.com>
2023-05-15 10:32:31 +0900

    HereIsTunnel 업로드

---
+++ HereIsTunnel/cyTunnel01_MonitoringRoom/Emergency.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cyTunnel01_MonitoringRoom
+{
+    /// <summary>
+    /// Emergency.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Emergency : MetroWindow
+    {
+        public Emergency()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCrash_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnOverturn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFire_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HereIsTunnel/cyTunnel01_MonitoringRoom/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace cyTunnel01_MonitoringRoom
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnsolution_Click(object sender, RoutedEventArgs e)
+        {
+            var subWin = new Emergency();
+            subWin.ShowDialog();
+        }
+
+        private void BtnLCD_Click(object sender, RoutedEventArgs e)
+        {
+            var subWin = new LcdControl();
+            subWin.ShowDialog();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            var subWin = new dontknow();
+            subWin.ShowDialog();
+        }
+
+        private void BtnGenDBControl_Click(object sender, RoutedEventArgs e)
+        {
+            var subWin = new DBmanagement();
+            subWin.ShowDialog();
+        }
+    }
+}
